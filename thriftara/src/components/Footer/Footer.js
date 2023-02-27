288cce5777a6bfc390cbd281d07a72eaa08e838d
Meggie <meggiecheng01@gmail.com>
2023-02-27 01:42:57 -0500

    fixed imports

--- thriftara/src/components/Footer/Footer.js
+++ thriftara/src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './Footer.css'
 import { Link } from 'react-router-dom'
 
-const Footer = () => {
+function Footer () {
   return (
     <>
       <div className='footer-container'>
--- thriftara/src/pages/Home.js
+++ thriftara/src/pages/Home.js
@@ -1,14 +1,10 @@
 import React from 'react'
 import '../styles/Home.css'
-import Navbar from '../components/Navbar/Navbar'
-import Footer from '../components/Footer/Footer'
 
 function Home() {
   return (
     <>
-    <Navbar />
     <div>Home Page Work in Progress ...</div>
-    <Footer />
     </>
   )
 }
