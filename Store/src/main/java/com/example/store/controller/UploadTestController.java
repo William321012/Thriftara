41b281bdbfc3c33078ffcef8ea988ca70d075102
chaochengzhu <70723055+chaochengzhu@users.noreply.github.com>
2023-03-10 12:22:27 -0500

    1

---
+++ Store/src/main/java/com/example/store/controller/UploadTestController.java
@@ -0,0 +1,66 @@
+package com.example.store.controller;
+
+import com.alibaba.fastjson.JSON;
+;
+import com.example.store.FileUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.*;
+
+@RestController
+public class UploadTestController {
+    @RequestMapping("/multipleImageUpload")
+    public List multipleImageUpload(@RequestParam("uploadFiles") MultipartFile[] files){
+        System.out.println("上传的图片数："+files.length);
+
+        List<Map<String,Object>> root=new ArrayList<Map<String,Object>>();
+
+        for (MultipartFile file : files) {    //循环保存文件
+
+            Map<String,Object> result=new HashMap<String, Object>();//一个文件上传的结果
+            String result_msg="";//上传结果信息
+
+            if (file.getSize() / 1000 > 100){
+                result_msg="图片大小不能超过100KB";
+            }
+            else{
+                //判断上传文件格式
+                String fileType = file.getContentType();
+                if (fileType.equals("image/jpeg") || fileType.equals("image/png") || fileType.equals("image/jpeg")) {
+                    // 要上传的目标文件存放的绝对路径
+//                    final String localPath="C:\\Users\\chao\\Documents\\classes\\static\\img";
+                    final String localPath= System.getProperty("user.dir")+"\\src\\main\\resources\\static\\images";
+                    //上传后保存的文件名(需要防止图片重名导致的文件覆盖)
+                    //获取文件名
+                    String fileName = file.getOriginalFilename();
+                    //获取文件后缀名
+                    String suffixName = fileName.substring(fileName.lastIndexOf("."));
+                    //重新生成文件名
+                    fileName = UUID.randomUUID()+suffixName;
+                    if (FileUtils.upload(file, localPath, fileName)) {
+                        //文件存放的相对路径(一般存放在数据库用于img标签的src)
+                        String relativePath="img/"+fileName;
+                        result.put("relativePath",relativePath);//前端根据是否存在该字段来判断上传是否成功
+                        result_msg="图片上传成功";
+                    }
+                    else{
+                        result_msg="图片上传失败";
+                    }
+                }
+                else{
+                    result_msg="图片格式不正确";
+                }
+            }
+            result.put("result_msg",result_msg);
+            root.add(result);
+        }
+        String root_json= JSON.toJSONString(root);
+        System.out.println(root_json);
+        return root;
+    }
+
+}
--- Store/src/main/java/com/example/store/controller/UserController.java
+++ Store/src/main/java/com/example/store/controller/UserController.java
@@ -14,6 +14,7 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
