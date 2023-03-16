c5a68d589508138aa7f3a47aeb64051784ba1657
William Lin <william.lin48@bcmail.cuny.edu>
2023-03-15 20:14:09 -0400

    Added Some More Component Templates

---
+++ thriftara/src/components/ForSale/ForSale.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class ForSale extends React.Component {
+  render() {
+    return (
+      <section>
+        <div></div>
+      </section>
+    );
+  }
+}
+
+export default ForSale;
---
+++ thriftara/src/components/Sold/Sold.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Sold extends React.Component {
+  render() {
+    return (
+      <section>
+        <div></div>
+      </section>
+    );
+  }
+}
+
+export default Sold;
