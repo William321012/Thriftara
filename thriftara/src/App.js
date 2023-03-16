ec6c85be6788eaa5f91e5ed211eb9272af166fc8
William Lin <william.lin48@bcmail.cuny.edu>
2023-03-15 20:10:19 -0400

    Merge Repository
    There are still conflicts we have to deal with, but overall it's fine.

--- thriftara/src/App.js
+++ thriftara/src/App.js
@@ -1,12 +1,16 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar/Navbar'
-import Footer from './components/Footer/Footer'
-import Home from './pages/Home';
-import About from './pages/About'
-import Brands from './pages/Brands';
-import FAQ from './pages/FAQ';
-import Cart from './pages/Cart';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import Brands from "./pages/Brands";
+import FAQ from "./pages/FAQ";
+import Cart from "./pages/Cart";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contacts from "./pages/Contacts";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Sell from "./pages/Sell";
 
 function App() {
   return (
@@ -14,11 +18,15 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/brands' element={<Brands />} />
-          <Route path='/faq' element={<FAQ />} />
-          <Route path='/cart' element={<Cart />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/brands" element={<Brands />} />
+          <Route path="/faq" element={<FAQ />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/pages/About" element={<About />} />
+          <Route path="/pages/Contacts" element={<Contacts />} />
+          <Route path="/pages/Login" element={<Login />} />
+          <Route path="/pages/Signup" element={<Signup />} />
+          <Route path="/pages/Sell" element={<Sell />} />
         </Routes>
         <Footer />
       </Router>
--- thriftara/src/components/Navbar/Navbar.js
+++ thriftara/src/components/Navbar/Navbar.js
@@ -1,57 +1,96 @@
-import React from 'react'
-import './Navbar.css'
-import Logo from '../../logo/logo.png'
-import { Link } from 'react-router-dom'
-import { BsCart4 } from 'react-icons/bs'
+import React from "react";
+import "./Navbar.css";
+import Logo from "../../logo/logo.png";
+import { Link } from "react-router-dom";
+import { BsCart4 } from "react-icons/bs";
 
 function Navbar() {
   return (
     <>
-      <div className='navbar-top'>
-        <Link to='/' className='nav-logo'><img src={Logo} alt='logo' /></Link>
-        <div className='search-bar'>
+      <div className="navbar-top">
+        <Link to="/" className="nav-logo">
+          <img src={Logo} alt="logo" />
+        </Link>
+        <div className="search-bar">
           <form>
-            <input type="text" placeholder='Search...' className='search-text' />
-            <button type='submit' className='search-btn'>Search</button>
+            <input
+              type="text"
+              placeholder="Search..."
+              className="search-text"
+            />
+            <button type="submit" className="search-btn">
+              Search
+            </button>
           </form>
         </div>
-        <ul className='topnav'>
-          <li><Link to='/sellnow'>Sell Now</Link></li>
-          <li><Link to='/login'>Login</Link></li>
-          <li><Link to='/signup'>Sign Up</Link></li>
-          <li><Link to='/cart'><BsCart4 size={20} /></Link></li>
+        <ul className="topnav">
+          <Link to="/pages/Sell">
+            <button>Sell</button>
+          </Link>
+          <Link to="/pages/Login">
+            <button>Login</button>
+          </Link>
+          <Link to="/pages/Signup">
+            <button>Signup</button>
+          </Link>
+          <li>
+            <Link to="/cart">
+              <BsCart4 size={20} />
+            </Link>
+          </li>
         </ul>
       </div>
-      <div className='navbar-btm'>
-        <ul className='bottomnav'>
-          <li className='brands'><Link to='/brands'>Brands
-            <div className='links'>
-              <ul className='drop'>
-                <li><Link to='https://www.nike.com'>Nike</Link></li>
-                <li><Link to='https://www.prada.com/us/en.html'>Prada</Link></li>
-                <li><Link to='https://us.supreme.com'>Supreme</Link></li>
-                <li><Link to='https://www.gucci.com/us/en/'>Gucci</Link></li>
-                <li><Link to='https://www.adidas.com/us'>Addidas</Link></li>
+      <div className="navbar-btm">
+        <ul className="bottomnav">
+          <li className="brands">
+            <Link to="/brands">
+              Brands
+              <div className="links">
+                <ul className="drop">
+                  <li>
+                    <Link to="https://www.nike.com">Nike</Link>
+                  </li>
+                  <li>
+                    <Link to="https://www.prada.com/us/en.html">Prada</Link>
+                  </li>
+                  <li>
+                    <Link to="https://us.supreme.com">Supreme</Link>
+                  </li>
+                  <li>
+                    <Link to="https://www.gucci.com/us/en/">Gucci</Link>
+                  </li>
+                  <li>
+                    <Link to="https://www.adidas.com/us">Addidas</Link>
+                  </li>
+                </ul>
+              </div>
+            </Link>
+          </li>
+          <li className="mens">
+            <Link to="/mens">
+              Menswear
+              <ul className="drop">
+                <li>Tops</li>
+                <li>Bottoms</li>
               </ul>
-            </div>
-          </Link></li>
-          <li className='mens'><Link to='/mens'>Menswear
-            <ul className='drop'>
-              <li>Tops</li>
-              <li>Bottoms</li>
-            </ul>
-          </Link></li>
-          <li className='womens'><Link to='/womens'>Womenswear
-            <ul className='drop'>
-              <li>Tops</li>
-              <li>Bottoms</li>
-            </ul>
-          </Link></li>
-          <li><Link to='/about'>About</Link></li>
+            </Link>
+          </li>
+          <li className="womens">
+            <Link to="/womens">
+              Womenswear
+              <ul className="drop">
+                <li>Tops</li>
+                <li>Bottoms</li>
+              </ul>
+            </Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
         </ul>
       </div>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
